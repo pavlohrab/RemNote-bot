09fafde156b0139a3bbdd4f860d01985bc745ffe
Pavlo Hrab <pavlo.hrab@lnu.edu.ua>
2021-02-19 20:27:27 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,47 @@
+# Background
+
+This is a telegram bot, that sends text notes to the daily documents (or in a separate folder, just tagging date) in [remnote](https://www.remnote.io/). If the text contains a link, then this link will be extracted and used as a source for rem. Any additional notes, following the created rem, are possible.
+
+The bot is intended to be run from [heroku](https://heroku.com) on a free tier. The installation guide below describes how to get the bot running.
+
+# Features
+1. **Send text notes** directly to the daily page, or in a separate folder, still referencing today's page
+2. **Share websites** to the remnote (the same as text notes). If a link is found within the text, then it becomes a source, and page title becomes the rem text
+3. **Add** any subsequent **notes to the created** rem (tight after the creatin). All the notes will put inside created rem. 
+4. **Tags** are only supported if they are one word ( underscore delimiter can be also used). In this case they can be added via ##example_tag while making a note.
+5. **References** are encapsulated in [[example reference]]. They can consist of several words. 
+
+**For example**: The one can share a link to a bot and follow it with several text messages, describing the ideas described in the website. Maybe "##Idea" can be tagged or [[example project]] referenced.
+
+# Installation
+
+1. Create a new bot via the @BotFather bot in Telegram. Just type `/newbot` command and follow the instructions
+2. Install [Git](https://git-scm.com/downloads) and [Heroku CLI](https://devcenter.heroku.com/articles/getting-started-with-python#set-up)
+3. Clone this repo locally and go to the folder. Just type in terminal (or git bash) `git clone && cd` 
+4. Then initialize an empty repo `git init`
+5. Create a Heroku app.
+   1. `heroku login` <- login to the Heroku CLI. If you don't have an account on Heroku, create one.
+   2. `heroku create` <- create an app
+6. Make API key for remnote (Make sure you registered in RemNote (; )). Please go [here](https://www.remnote.io/api_keys) and add a new API key. **Leave** "Enter the name of the top-level folder to restrict this API key to. (Leave this empty for full access.)" field **empty**. Then Name the Key as you wish. 
+7. Open `bot.py` file in any text editor (in a locally cloned copy of a repo) and add the following fields:
+   1. `TOKEN = ''` <- This is Telegram bot token. It must have been given from @BotFather bot. To remind it, you can list created bots via `/mybots`  -> `<your-bot-name>` -> `API Token` 
+   2. `REMNOT_API= ''` <- This is created RemNote API key (in step 6).
+   3. `USER_ID = ''` <- User Id can be obtained from [this](https://www.remnote.io/api_keys) page, where you have created your RemNote API. Copy the value from `User ID` column.
+   4. `HEROKU_NAME = ''` <- is the name of your Heroku app when you typed `heroku create`. Paste it in the format `https://yourherokuappname.herokuapp.com/`.
+   5. `HOME_DIR = 'Saved Telegram'` <- Name of a directory (rem) where to save content. You can provide any name you want. If no Rem will be found, it will be created
+8. Save the `bot.py` file. 
+9. From a terminal in a repo directory type `heroku git:remote -a YourAppName`, where your app name should be obtained from the created link - `https://yourherokuappname.herokuapp.com/`
+10. Type `git push heroku master`
+11. (optional) Go to the @BotFather bot in a Telegram. Go to the `/mybots`  -> `<your-bot-name>` -> `Edit Bot` -> `Edit Command` and type `stop - stop adding notes`. Then in the same `Edit Bot` menu go to the `Edit Botpic` and upload the picture you like. I used [this](https://drive.google.com/file/d/1_6PxFeHHWRDj26UIpuwcUsKam_FN6XSv/view) official one. All the official materials are available [here](https://www.remnote.io/a/remnote-media-kit/5fd4ff11c3785c0045946db7)
+12. Congrats, all is done!
+
+# Usage
+
+The only input, that is needed from the user, is a sent text message. Then the conversation dialogue via incline keyboard will begin:
+1. Firstly, the user can choose if to save note directly to the daily document, or save it separately
+2. Secondly, the user can add some following notes. You can add as many notes as you wish. Every message will be treated as separate rem. Links will not be extracted in this mode and would be treated as text. If you finished adding notes, then send the `/stop` command.
+
+Everything regarding how to use tags and references is in the **Features** section. If any API changes will be made, then this bot will change and evolve (:
+
+# What free Heroku tier means
+Because the bot is hosted on a free Heroku tier, it will go asleep after 30 min of inactivity. What that really means is that it can take up to 1 min for the bot to wake up after sending a message (after a long inactivity period). To my mind, it makes no big deal, but if it bothers you, then you can use [Kaffeine](https://kaffeine.herokuapp.com) app. But do not forget then to add some more free [dynos](https://www.heroku.com/dynos) (+400) via adding a credit card to your account! 
---
+++ bot.py
@@ -0,0 +1,261 @@
+# Import some libraries
+import logging
+from bs4 import BeautifulSoup 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
+from telegram.ext import *
+import os, json, requests, re
+from datetime import datetime as dt
+import datetime
+
+
+# USER defined variables
+TOKEN = ''
+REMNOT_API= ''
+USER_ID = ''
+HEROKU_NAME = ''
+HOME_DIR = 'Saved Telegram'
+
+# Some global variables
+PORT = int(os.environ.get('PORT', 5000))
+NOTE_ID = None
+PARENT = None
+NOTE = None
+MEDIA_ID = None
+FIRST, SECOND = range(2)
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+# Get the todays date!
+def suffix(d):
+    return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
+
+def custom_strftime(format, t):
+    return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
+
+today = custom_strftime('%B {S}, %Y', dt.now())
+
+# Send notes 
+def send_note(text, parent=None, link=False, doc=None):
+    global REMNOT_API, USER_ID, DOCUMENT, MEDIA_ID, today, URL
+    url_post="https://api.remnote.io/api/v0/create"
+    parent_Id = parent
+    if parent == None:
+        parent_Id = get_daily_rem()
+    if parent_Id == None:
+        parent_Id = search_parent()
+        text = text + "#[["+today+"]]"
+    par={
+    "apiKey": REMNOT_API,
+    "userId":USER_ID,
+    "text": text,
+    "parentId": parent_Id
+    }
+    if link == True:
+        par['isDocument'] = True
+        par['source'] = URL
+        reqs = requests.get(URL) 
+        soup = BeautifulSoup(reqs.text, 'html.parser') 
+        par['text'] =  soup.find_all('title')[0].get_text()
+    elif DOCUMENT==True:
+        par['source'] = open(f"{MEDIA_ID}.pdf", 'rb')
+        par['isDocument'] = True
+        par['text'] = MEDIA_ID
+    created_rem = requests.post(url_post, data=par)
+    return created_rem.json()['remId']
+
+def get_daily_rem():
+    global REMNOT_API, USER_ID
+    url_get_by_name = "https://api.remnote.io/api/v0/get_by_name"
+    search={
+    "apiKey": REMNOT_API,
+    "userId":USER_ID,
+    "name":datetime.date.today().strftime('%d/%m/%Y')}
+    res = requests.post(url_get_by_name, data=search)
+    if res.json()["found"] == True:
+        return res.json()["_id"]
+    else:
+        return None
+
+
+
+# Start the conversation 
+
+def start_doc(update, context):
+    global DOCUMENT, LINK, MEDIA_ID
+    DOCUMENT = True
+    LINK = False
+    media = context.bot.get_file(update.message.document)
+    photo = context.bot.get_file(media.file_id)
+    MEDIA_ID = media.file_id
+    photo.download(f"{media.file_id}.pdf")
+    if PARENT != None:
+        send_note(text=update.message.text, parent=PARENT, doc=media.file_id)
+    else:
+        update.message.reply_text("Need some extra details...",
+            reply_markup=main_menu_keyboard())
+        return FIRST
+
+def start(update, context):
+    """Echo the user message."""
+    global LINK, DOCUMENT, NOTE, PARENT, URL
+    DOCUMENT = False
+    NOTE = update.message.text
+    try:
+        text_to_search = str(NOTE).rsplit()
+        URL=None
+        for i in text_to_search:
+            if URL != None:
+                break
+            else:
+                try:
+                    URL = re.search("(?P<url>https?://[^\s]+)", str(i)).group("url")
+                except:
+                    pass
+        requests.get(URL)
+        LINK = True
+    except:
+        LINK = False
+    if PARENT != None:
+        send_note(text=update.message.text, parent=PARENT)
+    else:
+        update.message.reply_text("Need some extra details...",
+            reply_markup=main_menu_keyboard())
+        return FIRST
+
+def search_parent():
+    global HOME_DIR, REMNOT_API, USER_ID
+    name = HOME_DIR
+    url_get_by_name = "https://api.remnote.io/api/v0/get_by_name"
+    search={
+    "apiKey": REMNOT_API,
+    "userId":USER_ID,
+    "name":name}
+    res = requests.post(url_get_by_name, data=search)
+    if res.json()['found'] == True:
+        return res.json()['_id']
+    else:
+        url_post="https://api.remnote.io/api/v0/create"
+        par={
+            "apiKey": REMNOT_API,
+            "userId":USER_ID,
+            "text": name ,
+            "isDocument" : True
+            }
+        created_rem = requests.post(url_post, data=par)
+        return created_rem.json()['remId']
+
+# First menu hangling
+def daily_docs(update, context):
+    """Echo the user message."""
+    global LINK, DOCUMENT, NOTE, NOTE_ID    
+    query = update.callback_query
+    query.answer()
+    NOTE_ID = send_note(text=NOTE, link=LINK)
+    query.edit_message_text(text = first_menu_message(), 
+                        reply_markup=first_menu_keyboard())
+    return SECOND
+
+
+def separate_dir(update, context):
+    """Echo the user message."""
+    global LINK, DOCUMENT, NOTE, NOTE_ID, today
+    query = update.callback_query
+    query.answer()
+    NOTE_ID = send_note(text=NOTE + "#[["+today+"]]", link=LINK, parent=search_parent())
+    query.edit_message_text(text = first_menu_message(),
+                         reply_markup=first_menu_keyboard())
+    return SECOND
+
+
+# Second menu hangling
+def update_rem(update, context):
+    """Echo the user message."""
+    global PARENT, NOTE_ID
+    PARENT = NOTE_ID
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text("When you done adding notes, please type /stop command")
+    return ConversationHandler.END
+
+def stop_conv(update, context):
+    """Echo the user message."""
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text("Sure.")
+    return ConversationHandler.END
+
+# Stop notes adding
+def stop(update, context):
+    """Echo the user message."""
+    global PARENT
+    PARENT=None
+    DOCUMENT = False
+    LINK = False
+
+# Menus
+def main_menu(update, context):
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(main_menu_message(),
+                            reply_markup=main_menu_keyboard())
+    return FIRST
+
+
+def main_menu_keyboard():
+    keyboard = [[InlineKeyboardButton('Save to daily notes', callback_data='daily_docs')],
+                [InlineKeyboardButton('Save to separate dir', callback_data='separate_dir')]]
+    return InlineKeyboardMarkup(keyboard)
+
+def first_menu_keyboard():
+    keyboard = [[InlineKeyboardButton('Yes', callback_data='update_rem')],
+                [InlineKeyboardButton('No', callback_data='stop')]]
+    return InlineKeyboardMarkup(keyboard)
+
+def main_menu_message():
+  return 'Where to save?:'
+
+def first_menu_message():
+  return 'Add some additional notes? (inside created rem):'
+
+# Error messages
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def main():
+    global PARENT
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    conv_handler = ConversationHandler(
+        entry_points=[
+            MessageHandler(Filters.text, start), 
+            MessageHandler(Filters.document, start_doc)
+            ],
+        states={
+            FIRST: [
+                CallbackQueryHandler(main_menu, pattern='main'),
+                CallbackQueryHandler(daily_docs, pattern='daily_docs'),
+                CallbackQueryHandler(separate_dir, pattern='separate_dir'),
+            ],
+            SECOND: [
+                CallbackQueryHandler(update_rem, pattern="update_rem"),
+                CallbackQueryHandler(stop_conv, pattern="stop"),
+            ],
+        },
+        fallbacks=[CommandHandler('start', start)],
+    )
+    dp.add_handler(CommandHandler("stop", stop))
+    dp.add_handler(conv_handler)
+    dp.add_error_handler(error)
+#    updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",
+                            port=int(PORT),
+                            url_path=TOKEN)
+    updater.bot.setWebhook(HEROKU_NAME + TOKEN)
+    updater.idle()
+if __name__ == '__main__':
+    main()
