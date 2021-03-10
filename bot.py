1eede990331e8a982779b19ea21f380d3b013e1a
Pavlo Hrab <pavlo.hrab@lnu.edu.ua>
2021-03-10 14:16:40 +0200

    Fixed PORT issue

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ HEROKU_NAME = ''
 HOME_DIR = 'Saved Telegram'
 
 # Some global variables
-PORT = int(os.environ.get('PORT', 5000))
+PORT = os.environ.get('PORT')
 NOTE_ID = None
 PARENT = None
 NOTE = None
